817fd670b3f016c6f4e31f2cff7f86a1eb79d0b5
rizkym84 <36375488+rizkym84@users.noreply.github.com>
2019-12-26 23:00:27 +0700

    Kahji

---
+++ src/pages/404/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function NotFound(){
+    return <h1>Halaman Tidak Ditemukan</h1>
+}
+export default NotFound;
---
+++ src/pages/registrasi/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Registrasi(){
+    return <h1>Silakan Registrasi Terlebih Dahulu</h1>
+}
+
+export default Registrasi;
